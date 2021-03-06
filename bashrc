2b2504db82606a7bd766249c958268d4da7a3231
Neo Wie <it.brandonwie@gmail.com>
2021-03-06 20:05:15 +0800

    first commit

---
+++ bashrc
@@ -0,0 +1,4 @@
+export PATH=$PATH:/usr/local/bin
+export PS1="\[\033[38;5;191m\]\h@\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;220m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\\$ \[$(tput sgr0)\]"
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-15.jdk
+export PATH=$JAVA_HOME/bin:$PATH
---
+++ config.fish
@@ -0,0 +1,9 @@
+# Setting for Perl Error
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+
+# Setting PATH for Python 3.10
+# The original version is saved in /Users/brandonwie/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
+
+set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk-11.0.10.jdk/Contents/Home"
